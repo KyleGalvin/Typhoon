e8279dd76a48cbe763c744966008d2aeb359e83b
darkdigitaldream <darkdigitaldream@gmail.com>
2013-06-04 10:53:33 -0400

    adding raphaeltree project

---
+++ webDev/node/4multimediaLayers/server.js
@@ -0,0 +1,19 @@
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+var a = https.createServer(options, function (req, res) {
+  fs.readFile(__dirname + req.url, function (err,data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(8080);
