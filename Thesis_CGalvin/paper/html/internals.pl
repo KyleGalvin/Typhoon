f2596ff59094b4836a9c70c0876c26a26c2572fd
Kyle Galvin <darkdigitaldream@gmail.com>
2013-08-13 14:21:39 -0400

    adding more images

--- Thesis_CGalvin/paper/html/internals.pl
+++ Thesis_CGalvin/paper/html/internals.pl
@@ -3,99 +3,99 @@
 
 
 $key = q/cite_4297087/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_5054795/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6270872/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6182398/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_391769/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6001904/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_1376696/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_5397590/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_5340296/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_1032602/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_4698190/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6104414/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_5970856/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_4801602/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_1300874/;
-$ref_files{$key} = "$dir".q|node25.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_4469080/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_5710522/;
-$ref_files{$key} = "$dir".q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6094998/;
-$ref_files{$key} = "$dir".q|node25.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_6007847/;
-$ref_files{$key} = "$dir".q|node25.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_540147/;
-$ref_files{$key} = "$dir".q|node25.html|; 
+$key = q/cite_6094998/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4698190/;
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_4471983/;
-$ref_files{$key} = "$dir".q|node25.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_726589/;
-$ref_files{$key} = "$dir".q|node25.html|; 
+$key = q/cite_5710522/;
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_4359344/;
-$ref_files{$key} = "$dir".q|node25.html|; 
+$key = q/cite_1300874/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_5970856/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1376696/;
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_6296662/;
-$ref_files{$key} = "$dir".q|node25.html|; 
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_6270872/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_6182398/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_freudiger2007mix/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_5397590/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1032602/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_726589/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_391769/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_540147/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_5340296/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_5054795/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_6104414/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4801602/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4359344/;
+$ref_files{$key} = "$dir".q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 1;
--- Thesis_CGalvin/paper/html/labels.pl
+++ Thesis_CGalvin/paper/html/labels.pl
@@ -2,100 +2,100 @@
 # Associate labels original text with physical files.
 
 
-$key = q/cite_6270872/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_1376696/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_391769/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6001904/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_5397590/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6182398/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_4297087/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_5054795/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_4469080/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_1300874/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_5710522/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6094998/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6007847/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
+$key = q/cite_6104414/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_540147/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_726589/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_4471983/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6296662/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_4359344/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_5340296/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_1032602/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_4698190/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
+$key = q/cite_5710522/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_6104414/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
+$key = q/cite_6094998/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_4801602/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
+$key = q/cite_5397590/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_5340296/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4471983/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_freudiger2007mix/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_6296662/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_5054795/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4297087/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1300874/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1376696/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_391769/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_5970856/;
-$external_labels{$key} = "$URL/" . q|node25.html|; 
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4469080/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4359344/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_6270872/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_726589/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_6182398/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_6007847/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4698190/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4801602/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
 $noresave{$key} = "$nosave";
 
 1;
