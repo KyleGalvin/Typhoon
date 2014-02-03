d7552e5e8857033d7907c10af647fdbffa638230
Kyle Galvin <darkdigitaldream@gmail.com>
2014-02-03 16:17:53 -0500

    images and summary

--- Thesis_CGalvin/paper/html/contents.pl
+++ Thesis_CGalvin/paper/html/contents.pl
@@ -2,32 +2,32 @@
 # Associate contents original text with physical files.
 
 
-$key = q/0 0 3 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Server Side RESTful Model| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 3 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Bar Codes &amp; QR Codes| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
 $key = q/0 0 3 8 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
 $toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%P2P Connection| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
+$key = q/0 0 2 2 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Security| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Audio/Video Compression| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Digital Identification and Modeling| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 4 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%QR Code reading| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|mastersthesis.html%:%Implementation Details| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
 $key = q/0 0 4 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
 $toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Video Streaming| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 3 14 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%UI| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 4 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Authentication| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '2%:%'."$dir".q|mastersthesis.html%:%Contents| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
 $key = q/0 0 2 2 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
@@ -38,144 +38,144 @@ $key = q/0 0 3 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
 $toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Overview| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
-$key = q/0 0 3 12 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Message Passing| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 3 6 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Client Side Widget Factory| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 1 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Cellular Phones &amp; Mobile Collaboration| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 2 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Privacy| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 3 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Real-Time Digital Modeling| unless ($toc_section_info{$key}); 
+$key = q/0 0 3 12 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%I/O Limitations| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
 $key = q/0 0 3 12 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
 $toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Networking| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
-$key = q/0 0 3 12 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%I/O Limitations| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 3 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Server Side Command Dispatcher| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
 $key = q/0 0 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
 $toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Emerging Mobile Technologies| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
-$key = q/0 0 3 13 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Digital Signal Processing| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 2 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Cloud Based Assisted Technologies| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '2%:%'."$dir".q|mastersthesis.html%:%Designing OTA Mobile Telepresence Services| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 3 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Authentication Server| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 3 12 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%NAT traversal| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Digital Identification and Modeling| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '0%:%'."$dir".q|mastersthesis.html%:%Development of an OTA (Over the Air) Mobile Learning Telepresence Platform| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 3 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Goals| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 4 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Generic Video Capture| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 3 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Client Side Command Dispatcher| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 3 7 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Client Side Widget Structure| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 2 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Audio/Video Compression| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '2%:%'."$dir".q|mastersthesis.html%:%Lightweight Telepresence Technologies| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Telepresence &amp; Real-Time Communications| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 3 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%RFID; NFC| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 3 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Image Recognition and Classification| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Summary| unless ($toc_section_info{$key}); 
+$key = q/0 0 3 6 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Client Side Widget Factory| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
 $key = q/0 0 2 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
 $toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Microcontrollers &amp; customized System on a Chip Components| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
-$key = q/0 0 2 2 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Security| unless ($toc_section_info{$key}); 
+$key = q/0 0 2 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Summary| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
-$key = q/0 0 4 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%QR Code reading| unless ($toc_section_info{$key}); 
+$key = q/0 0 3 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Client Side Command Dispatcher| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
-$key = q/0 0 3 10 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Assumptions| unless ($toc_section_info{$key}); 
+$key = q/0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|mastersthesis.html%:%Contents| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
-$key = q/0 0 3 11 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Design| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 2 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Identifying the Major Elements for a Lightweight Mobile Telepresence System| unless ($toc_section_info{$key}); 
-$noresave{$key} = "$nosave";
-
-$key = q/0 0 4 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Event Handlers| unless ($toc_section_info{$key}); 
+$key = q/0 0 2 2 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Privacy| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
 $key = q/0 0 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
 $toc_section_info{$key} = '2%:%'."$dir".q|mastersthesis.html%:%Bibliography| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
-$key = q/0 0 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$toc_section_info{$key} = '2%:%'."$dir".q|mastersthesis.html%:%Implementation Details| unless ($toc_section_info{$key}); 
+$key = q/0 0 3 11 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Design| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Server Side Command Dispatcher| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 3 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Image Recognition and Classification| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 4 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Authentication| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 14 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%UI| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Server Side RESTful Model| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 12 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%NAT traversal| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Telepresence &amp; Real-Time Communications| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 3 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Bar Codes &amp; QR Codes| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 13 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Digital Signal Processing| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 12 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Message Passing| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 7 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Client Side Widget Structure| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|mastersthesis.html%:%Lightweight Telepresence Technologies| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Goals| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 2 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Cloud Based Assisted Technologies| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '0%:%'."$dir".q|mastersthesis.html%:%Development of an OTA (Over the Air) Mobile Learning Telepresence Platform| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 10 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Assumptions| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Authentication Server| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '2%:%'."$dir".q|mastersthesis.html%:%Designing OTA Mobile Telepresence Services| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 4 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Generic Video Capture| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 3 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%RFID; NFC| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 1 2 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Cellular Phones &amp; Mobile Collaboration| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 4 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Identifying the Major Elements for a Lightweight Mobile Telepresence System| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 2 3 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '4%:%'."$dir".q|mastersthesis.html%:%Real-Time Digital Modeling| unless ($toc_section_info{$key}); 
+$noresave{$key} = "$nosave";
+
+$key = q/0 0 4 5 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
+$toc_section_info{$key} = '3%:%'."$dir".q|mastersthesis.html%:%Event Handlers| unless ($toc_section_info{$key}); 
 $noresave{$key} = "$nosave";
 
 1;
--- Thesis_CGalvin/paper/html/internals.pl
+++ Thesis_CGalvin/paper/html/internals.pl
@@ -2,31 +2,7 @@
 # Associate internals original text with physical files.
 
 
-$key = q/cite_0001/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_freudiger2007mix/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_1300874/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6550384/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_5054795/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/cite_5710522/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_540147/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
@@ -38,11 +14,7 @@ $key = q/cite_6597703/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_1420259/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_1376696/;
+$key = q/cite_6550384/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
@@ -50,7 +22,7 @@ $key = q/cite_4117949/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_1032602/;
+$key = q/cite_1300874/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
@@ -58,7 +30,35 @@ $key = q/cite_6270872/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/cite_1420259/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_freudiger2007mix/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_5054795/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_0001/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_540147/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/cite_6094998/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1376696/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1032602/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
