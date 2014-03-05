9d5f124e3ac652edafbdb8423ec5dc5179aa8f8b
Kyle Galvin <darkdigitaldream@gmail.com>
2014-03-05 15:04:36 -0500

    latex code insertion

--- Thesis_CGalvin/paper/html/internals.pl
+++ Thesis_CGalvin/paper/html/internals.pl
@@ -2,35 +2,7 @@
 # Associate internals original text with physical files.
 
 
-$key = q/cite_5710522/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_726589/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6597703/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6550384/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_4117949/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_1300874/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_6270872/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_1420259/;
+$key = q/cite_5054795/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
@@ -38,15 +10,11 @@ $key = q/cite_freudiger2007mix/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_5054795/;
-$ref_files{$key} = "$dir".q|mastersthesis.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/cite_0001/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_540147/;
+$key = q/cite_1300874/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
@@ -54,11 +22,35 @@ $key = q/cite_6094998/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_1376696/;
+$key = q/cite_1032602/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_1032602/;
+$key = q/cite_6270872/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_4117949/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_5710522/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_6597703/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1420259/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_6550384/;
+$ref_files{$key} = "$dir".q|mastersthesis.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_1376696/;
 $ref_files{$key} = "$dir".q|mastersthesis.html|; 
 $noresave{$key} = "$nosave";
 
